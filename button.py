8e9b067f655ac8bb7fa287cdd0abcd4fd26bd93f
MJtsholo <tsholofelo.mautjana@umuzi.org>
2019-03-19 18:28:24 +0200

    changes to code

--- button.py
+++ button.py
@@ -2,55 +2,34 @@ import tkinter
 from graphics import *
 
 class Button:
-    """ A button is a labeled rectangle in a window. It is activated or deactivated
-    with the activate() and deactivate() methods. The clicked (p) method returns
-    true if the button is active and p is inside it. """
-
-def __init__ (self, win, center, width, height, label):
-        """creates a rectangualr button, eg:
-        qp = Button (myWin, centerPoint, width, height, 'quit')"""
-#set attributes here
-    
-        self.label = Text(label)
-        self.label.draw(win)
-        width = width/2.0
-        height = height/2.0
-        center.getX = x
-        center.getY = y
+    def __init__(self, win, center, width, height, label):
+        w,h = width/2.0, height/2.0
+        x,y = center.getX(), center.getY()
         self.xmax, self.xmin = x+w, x-w
-        self.ymax, self.tmin = y+h, y-h
-        p1 = Point(x-w, y-h)
-        p2 = Point(x+w, y+h)
-        self.square = Square(p1, p2)
-        self.square.setFill('blue')
-        self.square.draw(win)
+        self.ymax, self.ymin = y+h, y-h
+        p1 = Point(self.xmin, self.xmin)
+        p2 = Point(self.xmax, self.ymax)
+        self.rect = Rectangle(p1,p2)
+        self.rect.setFill('lightgray')
+        self.rect.draw(win)
+        self.label = Text(center, label)
+        self.label.draw(win)
         self.deactivate()
-
-def clicked(self, p):
-    """returns true if the button active and p is inside"""
-
-#CODE
-    return self.active
-           
-
-def getLabel(self):
-    """returns the label string of the button"""
-
-#CODE
-    return self.label.getText()
-
-def activate(self):
-    """Sets this button to 'active'"""
-
-#CODE
-    self.active = True
-    self.label.setFill('green')
-    self.square.setWidth(2)
-
-def deactivate (self):
-    """Sets this button to 'active'"""
+        
+    def clicked(self, p):
+        return self.active
+        self.xmin <= p.getX() <= self.xmax
+        self.ymin <= p.getY() <= self.ymax
+            
+    def getLabel(self):
+        return self.label.getText()
     
-    #CODE
-    self.active = False
-    self.label.setFill('red')
-    self.square.setWidth(1)																																																										
+    def activate(self):
+        self.label.setFill('black')
+        self.rect.setWidth(2)
+        self.active = True
+        
+    def deactivate(self):
+        self.label.setFill('darkgrey')
+        self.rect.setWidth(1)
+        self.active = False
--- roller.py
+++ roller.py
@@ -2,23 +2,23 @@ from random import randrange
 from graphics import GraphWin, Point
 
 from button import Button
-from dieview import DieView
+from dieview import DieView 
 
 def main():
 
-# create the application window
+    # create the application window
     win = GraphWin("Dice Roller")
-    win.setCoords(0,0,10,10)
+    win.setCoords(0, 0, 10, 10)
     win.setBackground("green2")
 
-# draw the interface widgets
+    # Draw the interface widgets
     die1 = DieView(win, Point(3,7), 2)
     die2 = DieView(win, Point(7,7), 2)
     rollButton = Button(win, Point(5,4.5), 6, 1, "Roll Dice")
     rollButton.activate()
     quitButton = Button(win, Point(5,1), 2, 1, "Quit")
 
-# event loop
+    # Event loop
     pt = win.getMouse()
     while not quitButton.clicked(pt):
         if rollButton.clicked(pt):
@@ -29,7 +29,7 @@ def main():
             quitButton.activate()
         pt = win.getMouse()
 
-# close the window
+    # close up shop
     win.close()
 
 main()
